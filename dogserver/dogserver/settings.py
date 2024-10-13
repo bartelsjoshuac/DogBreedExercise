4bb2b47698f36751a217f2695135136b9f12d1e8
bartelsjoshuac <bartelsjoshuac@gmail.com>
2024-10-13 16:56:46 -0600

    Reqconfigured for PostGres vs. SQLLite
    Reqconfigured for PostGres vs. SQLLite

--- dogserver/dogserver/settings.py
+++ dogserver/dogserver/settings.py
@@ -70,16 +70,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dogserver.wsgi.application'
 
-
+# Old SQL Lite config
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+#New PostGres Config
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dogserver',     
+        'USER': 'postgres',     
+        'PASSWORD': 'password',      
+        'HOST': 'localhost',              
+        'PORT': '5432',                   
     }
 }
+
 
 
 # Password validation
